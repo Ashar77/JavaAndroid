a1cacb7be3de56a9dfd7447365cede316622e6b3
ashar77 <asharmehmood360@gmail.com>
2021-06-06 20:06:08 +0500

    multiScreens

---
+++ MultiScreen/app/src/main/java/com/example/multiscreenapp/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.multiscreenapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView textView;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(MainActivity.EXTRA_NAME);
+
+        textView.setText("your name is "+ name);
+    }
+}
